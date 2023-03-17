51f49b84f9af6f85237ac934740fc4d618a53578
zyedibrahim <zyedibrahim77@gmail.com>
2023-03-17 23:49:41 +0530

    f

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-// export const API = "https://web-scrapping-ecommerce-node.vercel.app"
-export const API = "http://localhost:4000"
+export const API = "https://web-scrapping-ecommerce-node.vercel.app"
+// export const API = "http://localhost:4000"
